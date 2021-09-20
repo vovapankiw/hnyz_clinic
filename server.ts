700d74eeafe14747fccc6215cdffe812f44bcb1b
vovapankiw <vovapankiw@gmail.com>
2021-09-20 14:53:52 +0300

    changed path

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ export function app(): express.Express {
   const server = express();
   let distFolder = join(process.cwd(), "browser");
   if (!existsSync(distFolder)) {
-  distFolder = join(process.cwd(), "dist/YOUR_APP_NAME/browser");
+  distFolder = join(process.cwd(), "dist/clinic/browser");
   }
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
